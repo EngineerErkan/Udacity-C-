4ea47683d2855b969df6711752f621c9dfd09c98
Your Name <er_polat@hotmail.com>
2021-03-22 17:26:44 +0300

    Update processor.cpp

--- CppND-System-Monitor/src/processor.cpp
+++ CppND-System-Monitor/src/processor.cpp
@@ -6,30 +6,7 @@ float Processor::Utilization() {
   
   for (size_t i{0}; i<jiffies_list.size();i++){
     if (i!=LinuxParser::kGuest_ and i!=LinuxParser::kGuestNice_){
-    	total_jiffies +float Processor::Utilization() {
-  std::vector<std::string> jiffies = LinuxParser::CpuUtilization();
-  for (int i = 0; i < int(jiffies.size()); i++) {
-    if (i != LinuxParser::kGuest_ and i != LinuxParser::kGuestNice_) {
-      total_jiffies += std::stof(jiffies[i]);
-    }
-    if (i == LinuxParser::kIdle_ or i == LinuxParser::kIOwait_) {
-      idle_jiffies += std::stof(jiffies[i]);
-    }
-  }
-  util_jiffies = total_jiffies - idle_jiffies;
-
-  bool curr_util = true;
-  if (curr_util) {
-    usage = (util_jiffies - util_jiffies_prev) /
-            (total_jiffies - total_jiffies_prev);
-    total_jiffies_prev = total_jiffies;
-    util_jiffies_prev = util_jiffies;
-  } else {
-    usage = util_jiffies / total_jiffies;
-  }
-
-  return usage;
-}= std::stof(jiffies_list[i]);
+    	total_jiffies += std::stof(jiffies_list[i]);
     }
     if (i==LinuxParser::kIdle_ or i==LinuxParser::kIOwait_){
     	idle_jiffies += std::stof(jiffies_list[i]);
@@ -39,6 +16,8 @@ float Processor::Utilization() {
   
   bool curr_util = true;
   if (curr_util){
+//     std::cout << "util:" << util_jiffies << " prev:" << util_jiffies_prev << " substruct:" << util_jiffies-util_jiffies_prev << std::endl;
+//     std::cout << "util:" << total_jiffies << " prev:" << total_jiffies_prev << " substruct:" << total_jiffies-total_jiffies_prev << std::endl;
     usage = (util_jiffies-util_jiffies_prev)/(total_jiffies-total_jiffies_prev);
     total_jiffies_prev = total_jiffies;
     util_jiffies_prev = util_jiffies;
