2af32747800cb21520c53b50313738bde38084d8
Erkan <er_polat@hotmail.com>
2021-03-22 17:27:25 +0300

    Update processor.cpp

--- CppND-System-Monitor/src/processor.cpp
+++ CppND-System-Monitor/src/processor.cpp
@@ -3,7 +3,6 @@
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
   std::vector<std::string> jiffies_list = LinuxParser::CpuUtilization();
-  
   for (size_t i{0}; i<jiffies_list.size();i++){
     if (i!=LinuxParser::kGuest_ and i!=LinuxParser::kGuestNice_){
     	total_jiffies += std::stof(jiffies_list[i]);
@@ -16,8 +15,7 @@ float Processor::Utilization() {
   
   bool curr_util = true;
   if (curr_util){
-//     std::cout << "util:" << util_jiffies << " prev:" << util_jiffies_prev << " substruct:" << util_jiffies-util_jiffies_prev << std::endl;
-//     std::cout << "util:" << total_jiffies << " prev:" << total_jiffies_prev << " substruct:" << total_jiffies-total_jiffies_prev << std::endl;
+
     usage = (util_jiffies-util_jiffies_prev)/(total_jiffies-total_jiffies_prev);
     total_jiffies_prev = total_jiffies;
     util_jiffies_prev = util_jiffies;
